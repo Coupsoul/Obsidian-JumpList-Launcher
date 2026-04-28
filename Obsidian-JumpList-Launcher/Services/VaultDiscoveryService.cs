50758c6255b2ed9d32e3b60d5fb4a6054e752a3e
Coupsoul <slaw.kapustin2000@gmail.com>
2026-04-28 18:15:48 +0500

    feat: implement vault discovery logic

--- Obsidian-JumpList-Launcher/Services/VaultDiscoveryService.cs
+++ Obsidian-JumpList-Launcher/Services/VaultDiscoveryService.cs
@@ -1,4 +1,6 @@
-﻿using Obsidian_JumpList_Launcher.Models;
+﻿using System.IO;
+using System.Text.Json;
+using Obsidian_JumpList_Launcher.Models;
 
 namespace Obsidian_JumpList_Launcher.Services;
 
@@ -10,5 +12,31 @@ public class VaultDiscoveryService
     /// <summary>
     /// Читает файл %AppData%\obsidian\obsidian.json и извлекает список путей к хранилищам.
     /// </summary>
-    public IEnumerable<VaultInfo> GetVaults() => [];
+    public IEnumerable<VaultInfo> GetVaults()
+    {
+        string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string configPath = Path.Combine(appDataPath, "obsidian", "obsidian.json");
+        if (!File.Exists(configPath)) return [];
+
+        string jsonContent = File.ReadAllText(configPath);
+
+        var config = JsonSerializer.Deserialize<ObsidianConfig>(jsonContent);
+        if (config == null) return [];
+
+        var vaultsInfo = new List<VaultInfo>();
+
+        foreach (var vault in config.vaults)
+        {
+            string vaultPath = vault.Value.path;
+            string vaultName = Path.GetFileName(vaultPath);
+
+            vaultsInfo.Add(new VaultInfo(vaultName, vaultPath));
+        }
+
+        return vaultsInfo;
+    }
+
+    private record ObsidianVault(string path);
+
+    private record ObsidianConfig(Dictionary<string, ObsidianVault> vaults);
 }
